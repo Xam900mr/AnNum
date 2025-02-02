728d185512a7c23621156814a835334398971889
Max Rada <xam900mr@gmai.com>
2025-02-02 17:48:02 -0600

    pruebas 1

--- Proyecto 1 Montante.py
+++ Proyecto 1 Montante.py
@@ -1,6 +1,33 @@
+def metodo_montante(matriz, rest):
+    n = len(rest)
+    identidad = [[1 if i == j else 0 for j in range(n)]for i in range(n)]
+    print(identidad)
+
+    for k in range(n):
+        print(k)
+        actual = matriz[k][k] 
+        print (actual)
+        otras = [fila[k] for fila in matriz]
+        print(otras)
+        for j in range(n):
+            if k != j:
+                matriz[k][j] = 0
+        print(matriz)
+
+    pass
+
 def main():
+
+    matriz =([3, 6, -1],
+    [7, -1, 2],
+    [-2, -1, -1])
+
+    valores = [25, 9, -6]
+
+    metodo_montante(matriz, valores)
+
     
-    evaluador1 = False
+    """evaluador1 = False
     print("Escriba el tamaño de la matriz:")
     while evaluador1 == False:
         try:
@@ -23,13 +50,9 @@ def main():
                 except ValueError:
                     print("ERROR: Debe ingresar un numero. Intente nuevamente:")
                     
-        
+        """
                 
 
 if __name__ == "__main__":
     main()
 
-def metodo_monatnte(matrix, accesoria):
-
-    len(accesoria)
-    pass
