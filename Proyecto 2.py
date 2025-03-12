a58f6e14b980e8fa9ee8b309b4e113da5bf77e38
Enrikeso <124544287+Enrikeso@users.noreply.github.com>
2025-03-11 21:10:24 -0600

    Update Proyecto 2.py

--- Proyecto 2.py
+++ Proyecto 2.py
@@ -2,7 +2,10 @@ def Biergevieta(coeficientes,grado):
     a = coeficientes # Se copian los coeficientes del polinomio
     X = [0] * grado  # Se crea una lista para almacenar las raíces (inicialmente llena de ceros)
     for j in range(grado): #Iteracion para encontrar raices
-        X[j] = - a[-1] / a[-2]  # Primera aproximación de la raíz
+        try:
+            X[j] = - a[-1] / a[-2]  # Primera aproximación de la raíz
+        except ZeroDivisionError:
+            print("La funcion tiene soluciones imaginarias o no es apta para este metodo")
         while True:  #Se repite hasta encontrar la raiz
             P = [0] * len(a)
             Pprim = [0] * (len(a)-1)
@@ -39,8 +42,11 @@ def lectura_ecuacion():
         while True:
             try:
                 coef = float(input(f"Ingrese el coeficiente de x^{grado-i}: "))
-                coeficientes.append(coef)
-                break
+                if i == 0 and coef == 0:
+                    print(f"El valor de x^{i} debe ser distinto de 0")
+                else:    
+                    coeficientes.append(coef)
+                    break
             except ValueError:
                 print("Por favor, ingrese un número válido.")
     
