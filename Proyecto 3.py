4cf97ad1b8dfa0d494c5037b0c9c84f8e2c9d58c
Max Rada <xam900mr@gmai.com>
2025-04-01 16:32:38 -0600

    Proyecto 3 completo

--- Proyecto 3.py
+++ Proyecto 3.py
@@ -1,5 +1,26 @@
-def inter_lagrange():
-    return
+def inter_lagrange(tabla, x):
+    #Decalaramos una variable donde al amcenaremos la Y estimada por el proceso de interpolacion
+    Y_estimada = 0
+    #Declarsmos el numerador y el denominador de la division que se realizara
+    numerador = 1
+    denominador = 1
+    # Iniciamos con el proceso para obtener el numerador y denominador de cada fraccion que se va a a sumar
+    for i in range(len(tabla)):
+        for j in range(len(tabla)):
+            if i != j:
+                numerador = numerador * (x -tabla[j][0])
+        numerador = numerador * tabla[i][1]
+
+        for j in range(len(tabla)):
+            if i != j:
+                denominador = denominador * (tabla[i][0] -tabla[j][0])
+        #Sumamos la fraccion a la Y_estimada
+        Y_estimada += (numerador/denominador)
+        #Reiniciamos el numerador y el numerador para la proxima iteracion
+        numerador = 1
+        denominador = 1
+    #Retornamos el valor estimado de Y para X
+    return Y_estimada
 
 def lectura_datos():
     Datos = []
@@ -7,7 +28,7 @@ def lectura_datos():
     while True:
         while True:
             try:
-                x = float(input(f"Ingrese el valor de x{i+1}:"))
+                x = float(input(f"Ingrese el valor de x{i+1} = "))
                 if any(x == dato[0] for dato in Datos):  # Verifica si x ya existe en Datos
                     print("ERROR: El valor de x ya fue ingresado. Ingrese un valor diferente.")
                     continue
@@ -17,7 +38,7 @@ def lectura_datos():
 
         while True:
             try:
-                y = float(input(f"Ingrese el valor de y{i+1}:"))
+                y = float(input(f"Ingrese el valor de y{i+1} = "))
                 break
             except ValueError:
                 print("ERROR: ingrese un valor numerico")
@@ -26,16 +47,37 @@ def lectura_datos():
         i += 1
 
         if i >= 2:
-            print('¿Desea calcular otro polinomio?')
+            print('¿Desea ingresar otro conjunto de puntos?')
             opcion = input("Si / No\n")
             if opcion == 'si' or opcion == 'Si' or opcion == 'SI' or opcion == 'sI':
                 continue
             else:
                 break
     
-    return Datos
+    while True:
+        try:
+            X_estimar = float(input("Ingrese el valor de x para el cual se quiere interpolar el valor de y\n x = "))
+            if any(X_estimar == dato[0] for dato in Datos):  # Verifica si x ya existe en Datos
+                print("ERROR: El valor de x ya fue ingresado. Ingrese un valor diferente.")
+                continue
+            break
+        except ValueError:
+            print("ERROR: ingrese un valor numerico")
+    
+    
+    return Datos, X_estimar
 
 def main():
-    Tabla_Valores = lectura_datos()
+    while True:
+        Tabla_Valores, X_estimar = lectura_datos()
+        Y_estimada = inter_lagrange(Tabla_Valores, X_estimar)
+        print(f'El valor estimado de Y({X_estimar}) es de : {Y_estimada}')
+
+        print('¿Desea ingresar otros datos para interpolar?\n')
+        opcion = input("Si / No\n")
+        if opcion == 'si' or opcion == 'Si' or opcion == 'SI' or opcion == 'sI':
+            continue
+        else:
+            break
 if __name__ == "__main__":
     main()
